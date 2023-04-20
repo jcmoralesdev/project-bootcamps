f9d76428aaf8099e5e2f5f2f306e6113bef8bc90
jcmoralesdev <127900342+jcmoralesdev@users.noreply.github.com>
2023-04-20 10:18:46 -0600

    borrar comentario

--- src/components/PokemonDetail/Card.jsx
+++ src/components/PokemonDetail/Card.jsx
@@ -38,20 +38,3 @@ export default function Card({pokemon}) {
         </div>
         )
 }
-
-/*
-return (
-    <div style={{display: 'inline-block'}} className="mt-3">
-        <div className='card m-3 'style={{'background-color': '#ffcb05', 'box-shadow': '10px 10px 5px black'}}>
-            <div className='card-header' style={{'text-shadow': '3px 3px 3px black', "font-size":'7rem'}}>{onePokemon.name}</div>
-            <div className='card-body'>
-            <img src={onePokemon.sprites.front_default} alt="imagen-pokemon" style={{width: '10rem'}}></img>
-                <h1 className='card-text text-black' style={{'text-shadow': '4px 4px 2px black'}}>Datos Pokemon: </h1>
-                <p className='card-text ' style={{'text-shadow': '2px 2px 2px black'}}>Altura: {onePokemon.height} m</p>
-                <p className='card-text ' style={{'text-shadow': '2px 2px 2px black'}}>Peso: {onePokemon.weight} kg</p>
-                <p className='card-text ' style={{'text-shadow': '2px 2px 2px black'}}>Tipo: {onePokemon.types[0].type.name}</p>
-            </div>
-        </div>
-    </div>
-    )
-*/
